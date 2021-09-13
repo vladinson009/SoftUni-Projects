627bcd62249a869920bc1413573a1ff7db8014d5
vladinSon <vladinson009@icloud.com>
2021-09-13 21:49:19 +0200

    homework

--- JS-ADVANCED/01.Syntax-Functions-Statements-Exercise/03.sameNumbers.js
+++ JS-ADVANCED/01.Syntax-Functions-Statements-Exercise/03.sameNumbers.js
@@ -18,3 +18,25 @@ function sameNumbers(input) {
 }
 
 sameNumbers(2222222);
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+function same(input) {
+
+    const string = String(input);
+    let isSame = true;
+    let sum = 0;
+
+    for (let i = 0; i < string.length; i++) {
+        const currentNumber = Number(string[i])
+        const nextNumber = Number(string[i + 1])
+        if (currentNumber != nextNumber && !isNaN(nextNumber)) {
+            isSame = false;
+        }
+        sum += currentNumber;
+    }
+
+    console.log(isSame);
+    console.log(sum);
+}
+same(2222222)
