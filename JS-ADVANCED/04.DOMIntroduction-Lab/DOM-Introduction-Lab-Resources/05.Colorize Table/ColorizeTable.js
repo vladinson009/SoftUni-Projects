2a05fa0f03798ad727e42a1133b10a6f43d4e20e
vladinSon <vladinson009@icloud.com>
2021-10-02 22:02:17 +0200

    homework

--- JS-ADVANCED/04.DOMIntroduction-Lab/DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
+++ JS-ADVANCED/04.DOMIntroduction-Lab/DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+    const table = document.querySelectorAll('table tr:nth-child(even)');
+    // console.log(table);
+    for (let i = 0; i < table.length; i++) {
+        table[i].style = 'background-color: teal';
+
+    }
 }
--- JS-ADVANCED/04.DOMIntroduction-Lab/DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
+++ JS-ADVANCED/04.DOMIntroduction-Lab/DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
@@ -1,3 +1,9 @@
 function sumTable() {
-
+    let rows = document.querySelectorAll('table tr');
+    let sum = document.getElementById('sum');
+    let result = 0
+    for (let i = 1; i < rows.length - 1; i++) {
+        result += Number(rows[i].lastChild.textContent)
+    }
+    sum.textContent = result;
 }
