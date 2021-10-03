d4c2cf534043ec747af028cd9ca36d45beef0fa1
vladinSon <vladinson009@icloud.com>
2021-10-03 13:43:47 +0200

    homework

--- JS-ADVANCED/04.DOMIntroduction-Lab/DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
+++ JS-ADVANCED/04.DOMIntroduction-Lab/DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
@@ -3,7 +3,7 @@ function sumTable() {
     let sum = document.getElementById('sum');
     let result = 0
     for (let i = 1; i < rows.length - 1; i++) {
-        result += Number(rows[i].lastChild.textContent)
+        result += Number(rows[i].lastElementChild.textContent)
     }
     sum.textContent = result;
 }
--- JS-ADVANCED/04.DOMIntroduction-Lab/DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS-ADVANCED/04.DOMIntroduction-Lab/DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,14 @@
 function extract(content) {
+    let result = '';
 
+    const text = document.getElementById(content).textContent;
+    const pattern = /\((.+?)\)/g;
+    let match = pattern.exec(text);
+
+    while (match != null) {
+        result += match[1] + '; '
+
+        match = pattern.exec(text);
+    }
+    return result;
 }
