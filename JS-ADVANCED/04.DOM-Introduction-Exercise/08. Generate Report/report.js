b13c5baa65bf12bce23b06726315d4f15ce52f31
vladinSon <vladinson009@icloud.com>
2021-10-05 20:53:15 +0200

    homework

--- JS-ADVANCED/04.DOM-Introduction-Exercise/08. Generate Report/report.js
+++ JS-ADVANCED/04.DOM-Introduction-Exercise/08. Generate Report/report.js
@@ -1,3 +1,18 @@
 function generateReport() {
-    //TODO
+    const inputBoxes = document.querySelectorAll('tr th input');
+    const statistic = document.querySelectorAll('tbody tr');
+    const output = document.querySelector('#output');
+    const result = [];
+
+    for (let i = 0; i < statistic.length; i++) {
+        let currentObj = {};
+
+        for (let j = 0; j < inputBoxes.length; j++) {
+            if (inputBoxes[j].checked == true) {
+                currentObj[inputBoxes[j].name] = statistic[i].children[j].textContent;
+            }
+        }
+        result.push(currentObj);
+    }
+    output.textContent = JSON.stringify(result);
 }
