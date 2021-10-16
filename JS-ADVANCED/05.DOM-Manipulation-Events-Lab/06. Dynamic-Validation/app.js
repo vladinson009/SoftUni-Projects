eae8e1804d0b69d63090f13f14918bdcb08557e5
vladinSon <vladinson009@icloud.com>
2021-10-16 18:30:45 +0200

    homework

--- JS-ADVANCED/05.DOM-Manipulation-Events-Lab/06. Dynamic-Validation/app.js
+++ JS-ADVANCED/05.DOM-Manipulation-Events-Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    document.getElementById('email').addEventListener('change', onChange);
+
+    function onChange(ev) {
+        const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+        if (pattern.test(ev.target.value)) {
+            ev.target.classList.remove('error');
+        } else {
+            ev.target.classList.add('error');
+        }
+    }
 }
