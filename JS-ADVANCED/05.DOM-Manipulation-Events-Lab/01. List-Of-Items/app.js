343e33662fbf526b487fa73b6be46dad22a4ab8f
vladinSon <vladinson009@icloud.com>
2021-10-16 15:55:07 +0200

    homework

--- JS-ADVANCED/05.DOM-Manipulation-Events-Lab/01. List-Of-Items/app.js
+++ JS-ADVANCED/05.DOM-Manipulation-Events-Lab/01. List-Of-Items/app.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+    const items = document.getElementById('items');
+    const newItemText = document.getElementById('newItemText');
+
+    const li = document.createElement('li');
+    li.textContent = newItemText.value;
+    items.appendChild(li);
+    console.log(items);
+    newItemText.value = '';
 }
--- JS-ADVANCED/05.DOM-Manipulation-Events-Lab/02. Delete-From-Table/app.js
+++ JS-ADVANCED/05.DOM-Manipulation-Events-Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const contents = document.querySelectorAll('#customers tbody tr');
+    const input = document.querySelector('input[type="text"][name="email"]');
+    const result = document.getElementById('result');
+
+
+    for (let content of contents) {
+        const email = content.children[1].textContent;
+        if (input.value == email) {
+            content.remove()
+            result.textContent = 'Deleted.';
+            break;
+        }
+        result.textContent = 'Not found.';
+    }
+
 }
