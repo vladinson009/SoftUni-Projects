bcc0cdb8902d66d2bf0cce6a83d4c73648d1e07a
vladinSon <vladinson009@icloud.com>
2021-10-16 16:58:28 +0200

    homework

--- JS-ADVANCED/05.DOM-Manipulation-Events-Lab/03. Add-Delete/app.js
+++ JS-ADVANCED/05.DOM-Manipulation-Events-Lab/03. Add-Delete/app.js
@@ -1,3 +1,18 @@
 function addItem() {
-    //TODO...
+    const items = document.getElementById('items');
+    const newItemText = document.getElementById('newItemText');
+
+    const li = document.createElement('li');
+    li.textContent = newItemText.value;
+
+    const a = document.createElement('a')
+    a.textContent = '[Delete]';
+    a.href = '#';
+    a.addEventListener('click', (e) => e.target.parentElement.remove());
+
+    li.appendChild(a);
+    items.appendChild(li);
+
+    newItemText.value = '';
+
 }
--- JS-ADVANCED/05.DOM-Manipulation-Events-Lab/04. Mouse-Gradient/app.js
+++ JS-ADVANCED/05.DOM-Manipulation-Events-Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,11 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const result = document.getElementById('result');
+    const element = document.getElementById('gradient');
+    element.addEventListener('mousemove', onMove);
+
+    function onMove(ev) {
+        const box = ev.target;
+        const offset = Math.floor(ev.offsetX / box.clientWidth * 100)
+        result.textContent = offset + '%'
+    }
 }
