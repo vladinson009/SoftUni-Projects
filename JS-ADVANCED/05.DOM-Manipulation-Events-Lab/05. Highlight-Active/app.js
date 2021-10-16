88d1e4d90bd9cdcdcfc19e9cd569b0d6da679db8
vladinSon <vladinson009@icloud.com>
2021-10-16 17:13:52 +0200

    homework

--- JS-ADVANCED/05.DOM-Manipulation-Events-Lab/05. Highlight-Active/app.js
+++ JS-ADVANCED/05.DOM-Manipulation-Events-Lab/05. Highlight-Active/app.js
@@ -1,3 +1,17 @@
 function focused() {
-    console.log('TODO:...');
+
+    const fields = document.getElementsByTagName('input');
+
+    for (let field of fields) {
+        field.addEventListener('focus', onFocus);
+        field.addEventListener('blur', onBlur);
+    }
+
+    function onFocus(ev) {
+        ev.target.parentElement.className = 'focused';
+    }
+
+    function onBlur(ev) {
+        ev.target.parentElement.className = 'blured'
+    }
 }
