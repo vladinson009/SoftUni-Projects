67b2b6bba47062f5c790529150d5aa32c6776acd
vladinSon <vladinson009@icloud.com>
2021-10-17 17:54:13 +0200

    homework

--- JS-ADVANCED/05.DOM-Manipulation-Events-Lab/07. Shopping-Cart/solution.js
+++ JS-ADVANCED/05.DOM-Manipulation-Events-Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,26 @@
 function solve() {
-   //TODO...
+   document.querySelector('.shopping-cart').addEventListener('click', onClick);
+   const outputValue = document.getElementsByTagName('textarea')[0];
+   const allProducts = new Set();
+   const totalPrice = [];
+
+   function onClick(ev) {
+      if (ev.target.className == 'add-product') {
+         const parent = ev.target.parentElement.parentElement;
+         const product = parent.querySelector('.product-title').textContent;
+         const price = Number(parent.querySelector('.product-line-price').textContent);
+         outputValue.textContent += `Added ${product} for ${price.toFixed(2)} to the cart.\n`
+         allProducts.add(product);
+         totalPrice.push(price);
+      } else if (ev.target.className == 'checkout') {
+         const arrProducts = Array.from(allProducts.values())
+         outputValue.textContent += `You bought ${arrProducts.join(', ')} for ${totalPrice
+            .reduce((a, b) => a + b, 0).toFixed(2)}.`
+         const buttons = document.getElementsByTagName('button');
+         for (let button of buttons) {
+            button.disabled = true;
+            button.style.backgroundColor = 'gray';
+         }
+      }
+   }
 }
