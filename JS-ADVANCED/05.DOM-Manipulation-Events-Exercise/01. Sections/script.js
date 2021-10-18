9a62aee1cf25e13b98d0f09a50e10f1d9d4da7ba
vladinSon <vladinson009@icloud.com>
2021-10-18 21:30:20 +0200

    homework

--- JS-ADVANCED/05.DOM-Manipulation-Events-Exercise/01. Sections/script.js
+++ JS-ADVANCED/05.DOM-Manipulation-Events-Exercise/01. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+   const content = document.getElementById('content');
+   content.addEventListener('click', onClick);
+
+   for (let string of words) {
+      const divEl = document.createElement('div');
+      const pEl = document.createElement('p');
+      pEl.textContent = string;
+      pEl.style.display = 'none';
+      divEl.appendChild(pEl);
+      content.appendChild(divEl);
+
+   }
+
+   function onClick(ev) {
+      if (ev.target.tagName == 'DIV' && ev.target != content) {
+         ev.target.children[0].style.display = 'block';
+      }
+   }
 }
