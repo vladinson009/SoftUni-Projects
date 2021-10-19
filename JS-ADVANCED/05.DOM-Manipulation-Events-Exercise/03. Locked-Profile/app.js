721d404a752ba1661972ae85067997e021edf759
vladinSon <vladinson009@icloud.com>
2021-10-19 21:14:45 +0200

    homework

--- JS-ADVANCED/05.DOM-Manipulation-Events-Exercise/03. Locked-Profile/app.js
+++ JS-ADVANCED/05.DOM-Manipulation-Events-Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttons = Array.from(document.querySelectorAll('.profile button'));
+    buttons.forEach(b => b.addEventListener('click', toggleFunction));
+
+    function toggleFunction(e) {
+        const profile = e.target.parentElement;
+        const isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked;
+
+        if (isActive) {
+            const infoDiv = profile.querySelector('div');
+
+            if (e.target.textContent == 'Show more') {
+                infoDiv.style.display = 'block';
+                e.target.textContent = 'Hide it';
+            } else {
+                infoDiv.style.display = '';
+                e.target.textContent = 'Show more';
+            }
+        }
+    }
 }
