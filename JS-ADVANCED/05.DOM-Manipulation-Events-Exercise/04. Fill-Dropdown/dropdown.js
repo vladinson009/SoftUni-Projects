a9c6ff9801ba126ae4df0c25d3da60bdd6e6106e
vladinSon <vladinson009@icloud.com>
2021-10-19 21:35:52 +0200

    homework

--- JS-ADVANCED/05.DOM-Manipulation-Events-Exercise/04. Fill-Dropdown/dropdown.js
+++ JS-ADVANCED/05.DOM-Manipulation-Events-Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    const newItemText = document.getElementById('newItemText');
+    const newItemValue = document.getElementById('newItemValue');
+    const option = document.createElement('option');
+
+    option.textContent = newItemText.value;
+    option.value = newItemValue.value;
+    document.getElementById('menu').appendChild(option);
+    newItemText.value = '';
+    newItemValue.value = '';
+
 }
