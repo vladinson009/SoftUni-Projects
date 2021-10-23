f645ee1963ed2d20a753c7c7b25c0d6283415c7e
vladinSon <vladinson009@icloud.com>
2021-10-23 14:41:30 +0200

    homework

--- JS-ADVANCED/05.DOM-Manipulation-Events-Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS-ADVANCED/05.DOM-Manipulation-Events-Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,27 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const buttons = Array.from(document.querySelectorAll('button'));
+    buttons[0].addEventListener('click', sender)
+    buttons[1].addEventListener('click', reciever)
+    const senderValue = document.querySelectorAll('textarea')[0];
+    const recieverValue = document.querySelectorAll('textarea')[1];
+
+    function sender() {
+        let result = '';
+        for (let i = 0; i < senderValue.value.length; i++) {
+            const symAsChar = senderValue.value.charCodeAt(i) + 1;
+            result += String.fromCharCode(symAsChar);
+        }
+        recieverValue.value = result;
+        senderValue.value = '';
+    }
+
+    function reciever() {
+        let result = '';
+        for (let i = 0; i < recieverValue.value.length; i++) {
+            const symAsChar = recieverValue.value.charCodeAt(i) - 1;
+            result += String.fromCharCode(symAsChar);
+        }
+        recieverValue.value = result;
+    }
+
 }
--- JS-ADVANCED/05.DOM-Manipulation-Events-Exercise/06. Furniture/solution.js
+++ JS-ADVANCED/05.DOM-Manipulation-Events-Exercise/06. Furniture/solution.js
@@ -1,4 +1,68 @@
 function solve() {
 
-  //TODO...
+  const [generateBtn, buyBtn] = document.querySelectorAll('button');
+  const [input, output] = document.querySelectorAll('textarea');
+  const table = document.querySelector('table.table tbody');
+
+  generateBtn.addEventListener('click', generate);
+  buyBtn.addEventListener('click', buy);
+
+  function buy(e) {
+    const markedFurniture = table.querySelectorAll('input[type="checkbox"]:checked');
+    const names = [];
+    let totalPrice = 0;
+    let totalDecFac = 0;
+    for (let product of markedFurniture) {
+      const row = product.parentElement.parentElement;
+      const productName = row.children[1].textContent;
+      const productPrice = Number(row.children[2].textContent);
+      const decorationFactor = Number(row.children[3].textContent);
+      totalPrice += productPrice;
+      totalDecFac += decorationFactor;
+      names.push(productName);
+    }
+    output.value = `Bought furniture: ${names.join(', ')}
+Total price: ${totalPrice.toFixed(2)}
+Average decoration factor: ${totalDecFac / markedFurniture.length}`
+  }
+
+  function generate(e) {
+    const data = JSON.parse(input.value);
+
+    for (let furniture of data) {
+      const row = document.createElement('tr');
+
+      const imgCell = document.createElement('td');
+      const img = document.createElement('img');
+      img.src = furniture.img;
+      imgCell.appendChild(img);
+      row.appendChild(imgCell);
+
+      const furnitureName = document.createElement('td');
+      const nameP = document.createElement('p');
+      nameP.textContent = furniture.name;
+      furnitureName.appendChild(nameP);
+      row.appendChild(furnitureName)
+
+      const furniturePrice = document.createElement('td');
+      const priceP = document.createElement('p');
+      priceP.textContent = furniture.price;
+      furniturePrice.appendChild(priceP);
+      row.appendChild(furniturePrice);
+
+      const decFactor = document.createElement('td');
+      const decP = document.createElement('p');
+      decP.textContent = furniture.decFactor;
+      decFactor.appendChild(decP);
+      row.appendChild(decFactor);
+
+      const checkbox = document.createElement('td');
+      const checkInput = document.createElement('input');
+      checkInput.type = 'checkbox';
+      checkbox.appendChild(checkInput);
+      row.appendChild(checkbox);
+      ////////////
+      table.appendChild(row);
+    }
+  }
 }
