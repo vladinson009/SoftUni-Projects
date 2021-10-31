48712e62c00c803bc58243cf9688337abbdfcfd5
vladinSon <vladinson009@icloud.com>
2021-10-31 22:38:27 +0100

    homework

---
+++ JS-ADVANCED/06.Advanced-Functions-Exercise/01.SortArray.js
@@ -0,0 +1,13 @@
+function sortArray(arr, string) {
+
+    function sorting(a, b) {
+        if (string == 'asc') {
+            return a - b;
+        } else if (string == 'desc') {
+            return b - a;
+        }
+    }
+    return arr.sort(sorting);
+}
+
+console.log(sortArray([14, 7, 17, 6, 8], 'asc'));
